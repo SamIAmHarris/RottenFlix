61d4b47254eff08612995f441f38322e89b1bcc1
Sam Harris <samh@jackrabbitmobile.com>
2016-04-14 12:50:20 -0500

    initial commit

---
+++ app/src/main/java/samiamharris/samlearn/MainActivity.java
@@ -0,0 +1,13 @@
+package samiamharris.samlearn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
